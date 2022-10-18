d051a407fb50c848d77d558cb49ed999dd045b51
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2022-10-18 17:14:57 +0530

    demo: useEffect, useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  useEffect(() => {
+    console.log('it will get called always');
+  });
+
+  useEffect(() => {
+    console.log('counter1');
+  }, [counter]);
+
+  useEffect(() => {
+    console.log('counter2');
+  }, [counter2]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          counter 1: { counter }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>counter 2: { counter2 }</p>
+        <button onClick={() => setCounter(counter + 1)}>increment counter1</button>
+        <button onClick={() => setCounter2(counter2 + 1)}>increment counter2</button>
       </header>
     </div>
   );
